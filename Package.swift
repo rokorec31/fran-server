94c614b439b06e3f0fb80935de03c0613d15962a
HungI <i.hung@athentek.com>
2017-06-07 15:25:22 +0800

    first commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Fran",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/sqlite.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/fluent.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/sqlite-driver.git", majorVersion: 1),
+        .Package(url:"https://github.com/matthijs2704/vapor-apns.git", majorVersion: 1, minor: 2)
+    ],
+    exclude: [
+        "Database",
+        "Resources",
+        "Public",
+        "Tests"
+    ]
+)
